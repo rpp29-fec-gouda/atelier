c0e62f97ff36a362efdc9821d70242ce2beec4b1
Steven Harder <stevenharderjr@gmail.com>
2021-07-18 15:17:44 -0600

    Demonstrates communication between client and API

---
+++ client/src/index.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+import './style.css';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      products: []
+    };
+  }
+
+  handleUpdate(urlToReload, stateKeyToUpdate) {
+    axios.get(urlToReload)
+      .then(res => {
+        this.setState({
+          [stateKeyToUpdate]: res.data
+        });
+      })
+      .catch(err => {
+        console.log(err.stack);
+      });
+  }
+
+  componentDidMount() {
+    this.handleUpdate('/products', 'products');
+  }
+
+  render() {
+    const { products } = this.state;
+    return (
+      <div id='App'>
+        <h3>Behold, some data:</h3>
+        {
+          products.length ? (
+            products.map(product => (<div key={product.id}>{product.name}</div>))
+          ) : (
+            <p>Loading...</p>
+          )
+        }
+      </div>
+    );
+  }
+}
+
+const div = document.createElement('div');
+div.setAttribute('id', 'Atelier');
+document.body.appendChild(div);
+ReactDOM.render(<App />, div);
