4de1d9c275f9883d4d158ed008c8742947f2202d
Steven Harder <stevenharderjr@gmail.com>
2021-07-21 21:40:07 -0600

    I don't know why, but git seems insistent that I commit these files

--- tests/demo/identify.js
+++
@@ -1,3 +0,0 @@
-const identify = () => ('All I do is say "test", in order to identify myself as a test.');
-
-module.exports = identify;
--- tests/demo/identify.test.js
+++
@@ -1,9 +0,0 @@
-const identify = require('./identify.js');
-
-test('Identifies as a test', () => {
-  expect(identify()).toContain('I');
-  expect(identify()).toContain('identify');
-  expect(identify()).toContain('as');
-  expect(identify()).toContain('a');
-  expect(identify()).toContain('test');
-});
