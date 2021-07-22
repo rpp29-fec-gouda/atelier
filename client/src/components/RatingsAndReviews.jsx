e24fbd5dda28891bda967af120f8b5fed8815753
Aleksandar Cakic <acakic92@gmail.com>
2021-07-22 02:00:03 -0500

    Tickets Review: Ratings and Reviews module

---
+++ client/src/components/RatingsAndReviews.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+class RatingsAndReviews extends React.Component {
+  render () {
+    return (<div>Ratings and Reviews</div>)
+  }
+}
+
+export default RatingsAndReviews;
---
+++ tests/client/src/components/ratingsAndReviews.test.js
@@ -0,0 +1,32 @@
+import Enzyme from 'enzyme';
+import {mount, shallow} from 'enzyme'
+import React from 'react'
+import RatingsAndReviews from '../../../client/src/components/RatingsAndReviews.jsx'
+
+// ==== Test Template ====
+// ====== index.jsx ======
+// For Jest usage, see: https://jestjs.io/docs/getting-started
+// For Enzyme usage, see: https://github.com/enzymejs/enzyme-matchers/tree/master/packages/jest-enzyme
+
+describe('<RatingsAndReviews />', () => {
+  test('renders product information', () => {
+    // const wrapper = mount(<RatingsAndReviews />); // mount/render/shallow when applicable
+
+    // // Assert that the given enzyme wrapper has rendered content.
+    // expect(wrapper.find('div')).toBeTruthy();
+    // expect(wrapper.find('h3')).toBeTruthy();
+    // expect(wrapper.find('ul')).toEqual({});
+  });
+
+  test('renders review list', () => {
+
+  });
+
+  test('renders rating breakdown', () => {
+
+  });
+
+  test('renders product breakdown', () => {
+
+  });
+});
