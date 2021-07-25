ef18b96096cf2ac61ba86a07ebef718e3e8d39eb
Steven Harder <stevenharderjr@gmail.com>
2021-07-25 07:28:07 -0600

    Resolves merge conflicts between related products feature and test suite

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -46,11 +46,7 @@ class App extends React.Component {
   componentDidMount() {
     this.handleUpdate('/products', 'products')
       .then(() => {
-<<<<<<< HEAD
         this.selectProduct(this.state.products[0]);
-=======
-        const initialProductId = this.state.products[0]?.id;
->>>>>>> 9ad52647eeba71387bb3324365316a8eb50d05e1
         this.setState({
           ready: true
         });
