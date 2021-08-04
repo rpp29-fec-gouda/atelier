af0260e889f1a3a120f3fef1072220659a688c88
Mark P Thomas <markums@gmail.com>
2021-08-03 22:50:03 -0600

    Steve's solution is better :-P

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const app = express();
 
 app.use(express.static(path.join(__dirname, '/client/dist')));
+app.use(express.static(path.join(__dirname, '/client/assets')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Display any parameters appended to the URL:
