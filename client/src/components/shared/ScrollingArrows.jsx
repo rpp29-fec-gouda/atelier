8a38d8bfee6784a93c911bc61ee098e271e705ae
Mark P Thomas <markums@gmail.com>
2021-08-04 01:02:21 -0600

    Refactored incrementor component to have logic outside of the component

--- client/src/components/shared/ScrollingArrows.jsx
+++ client/src/components/shared/ScrollingArrows.jsx
@@ -1,41 +1,63 @@
 import React from 'react';
+import { incrementIndexUp, incrementIndexDown } from '../../model/Calcs';
 import './scrollingArrows.css';
 
 // props: index, isCapped, max, min, stem
-const ScrollingArrows = (props) => {
-  let index = props.index ? props.index : 0;
-  let min = props.min ? props.min : 0;
-  let decrementClass = 'decrement';
-  decrementClass += props.stem ? ' decrement-stem' : ' decrement-no-stem';
-  let incrementClass = 'increment';
-  incrementClass += props.stem ? ' increment-stem' : ' increment-no-stem';
+class ScrollingArrows extends React.Component {
+  constructor(props) {
+    super(props);
 
-  const incrementIndexUp = () => {
-    index++;
-    if (props.max && index >= props.max) {
-      index = props.isCapped ? props.max : 0;
-    }
-    props.onClickIncrement(index);
-  };
+    this.state = {
+      index: props.index ? props.index : 0
+    };
 
-  const incrementIndexDown = () => {
-    index--;
-    if (index < min) {
-      if (props.isCapped) {
-        index = props.min;
-      } else if (props.max) {
-        index = props.max - 1;
-      }
-    }
-    props.onClickDecrement(index);
-  };
+    this.min = props.min ? props.min : 0;
 
-  return (
-    <div class="scrolling-arrows">
-      <div class={decrementClass} onClick={incrementIndexDown}></div>
-      <div class={incrementClass} onClick={incrementIndexUp}></div>
-    </div>
-  );
-};
+    this.decrementClass = 'decrement';
+    this.decrementClass += props.stem ? ' decrement-stem' : ' decrement-no-stem';
+
+    this.incrementClass = 'increment';
+    this.incrementClass += props.stem ? ' increment-stem' : ' increment-no-stem';
+
+    this.incrementIndexUp = this.incrementIndexUp.bind(this);
+    this.incrementIndexDown = this.incrementIndexDown.bind(this);
+  }
+
+  incrementIndexUp() {
+    let index = this.state.index;
+    let max = this.props.max;
+    let isCapped = this.props.isCapped;
+
+    index = incrementIndexUp(index, max, isCapped);
+
+    this.props.onClickIncrement(index);
+    this.setState({
+      index: index
+    });
+  }
+
+  incrementIndexDown() {
+    let index = this.state.index;
+    let max = this.props.max;
+    let min = this.min;
+    let isCapped = this.props.isCapped;
+
+    index = incrementIndexDown(index, max, min, isCapped);
+
+    this.props.onClickDecrement(index);
+    this.setState({
+      index: index
+    });
+  }
+
+  render() {
+    return (
+      <div class="scrolling-arrows">
+        <div class={this.decrementClass} onClick={this.incrementIndexDown}></div>
+        <div class={this.incrementClass} onClick={this.incrementIndexUp}></div>
+      </div>
+    );
+  }
+}
 
 export default ScrollingArrows;
