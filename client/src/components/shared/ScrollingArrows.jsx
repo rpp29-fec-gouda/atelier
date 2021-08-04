607508bbddb96af3e79406fbbfe510b606c7aa02
Mark P Thomas <markums@gmail.com>
2021-08-03 23:53:45 -0600

    Added icons. Also refactored to better abstract and make component more general

--- client/src/components/shared/ScrollingArrows.jsx
+++ client/src/components/shared/ScrollingArrows.jsx
@@ -1,11 +1,39 @@
 import React from 'react';
 import './scrollingArrows.css';
 
+// props: index, isCapped, max, min, stem
 const ScrollingArrows = (props) => {
+  let index = props.index ? props.index : 0;
+  let min = props.min ? props.min : 0;
+  let decrementClass = 'decrement';
+  decrementClass += props.stem ? ' decrement-stem' : ' decrement-no-stem';
+  let incrementClass = 'increment';
+  incrementClass += props.stem ? ' increment-stem' : ' increment-no-stem';
+
+  const incrementIndexUp = () => {
+    index++;
+    if (props.max && index >= props.max) {
+      index = props.isCapped ? props.max : 0;
+    }
+    props.onClickIncrement(index);
+  };
+
+  const incrementIndexDown = () => {
+    index--;
+    if (index < min) {
+      if (props.isCapped) {
+        index = props.min;
+      } else if (props.max) {
+        index = props.max - 1;
+      }
+    }
+    props.onClickDecrement(index);
+  };
+
   return (
     <div class="scrolling-arrows">
-      <div class="left-arrow">&lt;</div>
-      <div class="right-arrow">&gt;</div>
+      <div class={decrementClass} onClick={incrementIndexDown}></div>
+      <div class={incrementClass} onClick={incrementIndexUp}></div>
     </div>
   );
 };
