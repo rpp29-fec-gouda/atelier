a134515c59fa4f33a0b21bd4d74c96f9a75f720d
Mark P Thomas <markums@gmail.com>
2021-08-06 10:25:07 -0600

    Fell behind on commits with these! Will do better moving forward. Significant development in default view, main image, image navigator

--- client/src/components/overview/DefaultView.jsx
+++ client/src/components/overview/DefaultView.jsx
@@ -1,21 +1,32 @@
 import React from 'react';
 import ScrollingArrows from '../shared/ScrollingArrows';
 import ImageNavigator from './ImageNavigator';
-import MainImage from './MainImage';
 
 const DefaultView = (props) => {
   console.log('Rendering default view');
+  console.log('selectedId', props.selectedId);
+
   return (
     <div id="default-view">
       Default View
       <div class="row">
-        <ImageNavigator />
+        <ImageNavigator
+          onClick={props.onClickIndexUpdate}
+          photos={props.thumbnails}
+          selectedId={props.selectedId}
+        />
         <div class="column">
-          <div class="expanded-view" onClick={props.onClick}>[ ]</div>
-          <ScrollingArrows />
+          <div class="expanded-view-toggle" onClick={props.onClickImage}>[+]</div>
+          <ScrollingArrows
+            callback={props.onClickIndexUpdate}
+            max={props.thumbnails?.length - 1}
+            stem={true}
+          />
         </div>
       </div>
-      {/* <MainImage /> */}
+      <div id="main-image">
+        <img src={props.photo} />
+      </div>
     </div>
   );
 };
