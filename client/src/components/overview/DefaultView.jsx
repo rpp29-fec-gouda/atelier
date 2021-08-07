530c88a03325a4de6036aa026b86a8e8e8f95949
Mark P Thomas <markums@gmail.com>
2021-08-07 13:23:46 -0600

    Updating incomplete Image Gallery component & children in order to patch a break due to SVG files referenced. SVG files are now inlined in the HTML to avoid this.

--- client/src/components/overview/DefaultView.jsx
+++ client/src/components/overview/DefaultView.jsx
@@ -10,13 +10,20 @@ const DefaultView = (props) => {
     <div id="default-view">
       Default View
       <div class="row">
-        <ImageNavigator
+        {/* <ImageNavigator
           onClick={props.onClickIndexUpdate}
           photos={props.thumbnails}
           selectedId={props.selectedId}
-        />
+        /> */}
         <div class="column">
-          <div class="expanded-view-toggle" onClick={props.onClickImage}>[+]</div>
+          <div class="expanded-view-toggle" onClick={props.onClickImage}>
+            <svg viewBox="0 0 20 20">
+              <path
+                fill-rule="evenodd"
+                d="M1 1v6h2V3h4V1H1zm2 12H1v6h6v-2H3v-4zm14 4h-4v2h6v-6h-2v4zm0-16h-4v2h4v4h2V1h-2z"
+              />
+            </svg>
+          </div>
           <ScrollingArrows
             callback={props.onClickIndexUpdate}
             max={props.thumbnails?.length - 1}
