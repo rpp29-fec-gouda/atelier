8f6380ac78fc8a39e9ae05addaf64f4f8587d2a6
Steven Harder <stevenharderjr@gmail.com>
2021-08-21 08:17:51 -0600

    Confines initial load to lower product ids

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -113,7 +113,7 @@ class App extends React.Component {
 
 export default App;
 
-const randomPage = Math.round(Math.random() * 900);
+const randomPage = Math.round(Math.random() * 10);
 axios.get(`/products?page=${randomPage}&count=1`)
   .then(res => {
     const product = res.data[0];
