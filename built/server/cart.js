571b645d40513272c3ddcd2df5a6b7dab170fa23
Mark P Thomas <markums@gmail.com>
2022-06-16 10:48:10 -0600

    Updated config file, ignoring Typescript build output

--- built/server/cart.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.cart = void 0;
-const api_1 = require("./api");
-const endpoint = '/cart';
-exports.cart = {
-    getCart: () => api_1.requests.get(endpoint),
-    addToCart: (sku) => api_1.requests.post(endpoint, { "sku_id": sku })
-};
